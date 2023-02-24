37ad05ac97c3c302b5d56c37636cb0236dded077
Paul Zoltán <paulzoltan02@gmail.com>
2023-02-24 12:41:47 +0100

    improve backround behavior

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,22 @@
 import './layout.css'
+import Header from '../Header'
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className='layout'>
-    <div className='layout__bg-contanier layout__bg-contanier--left'></div>
-    <div className='layout__crumple-zone layout__crumple-zone--left'></div>
+    <div className='layout__bg-contanier--outer '>
+      <div className='layout__bg-contanier--left--inner'></div>
+    </div>
+    <div className='layout__crumple-zone'></div>
     <div className='layout__container'>
-      <div className='layout__header'>header</div>
+      <Header />
+      <div className='negative-space'></div>
       {children}
       <div className='layout__footer'>footer</div>
     </div>
-    <div className='layout__crumple-zone layout__crumple-zone--right'></div>
-    <div className='layout__bg-contanier layout__bg-contanier--right'></div>
+    <div className='layout__crumple-zone'></div>
+    <div className='layout__bg-contanier--outer '>
+      <div className='layout__bg-contanier--right--inner'></div>
+    </div>
   </div>
 )
 export default Layout
