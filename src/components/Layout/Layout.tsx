71ac9f9f746e7906bd6296310110adfba45cfd7d
Paul Zoltán <paulzoltan02@gmail.com>
2023-02-25 22:40:32 +0100

    loader, preload fonts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,12 +2,19 @@ import './layout.css'
 import Header from './Header'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
+import { motion } from 'framer-motion'
+
+const motionProps = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  transition: { duration: 0.4, delay: 0.3 },
+}
 
 const Layout = () => (
   <div className='layout'>
-    <div className='layout__bg-contanier--outer '>
+    <motion.div {...motionProps} className='layout__bg-contanier--outer'>
       <div className='layout__bg-contanier--left--inner'></div>
-    </div>
+    </motion.div>
     <div className='layout__crumple-zone'></div>
     <div className='layout__container'>
       <Header />
@@ -15,9 +22,9 @@ const Layout = () => (
       <Footer />
     </div>
     <div className='layout__crumple-zone'></div>
-    <div className='layout__bg-contanier--outer '>
+    <motion.div {...motionProps} className='layout__bg-contanier--outer '>
       <div className='layout__bg-contanier--right--inner'></div>
-    </div>
+    </motion.div>
   </div>
 )
 export default Layout
